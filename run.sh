d3e228f7016b890d10eabcf51f98b1663f830f9b
Nilson Lazarin <nilsonmori@yahoo.com>
2025-01-27 19:50:32 -0300

    ajustes remoção de arquivo

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 clear
 echo "Iniciando execução..."
 sleep 3
-rm Bot-AxeDevTools/insumo-bot-axe.xlsx 
+#rm Bot-AxeDevTools/insumo-bot-axe.xlsx 
 source pythonENV/bin/activate
 cd Bot-Varredura/
 python3 main.py
